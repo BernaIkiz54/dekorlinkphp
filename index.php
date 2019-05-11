11be222590161b0096cd71de0eebeb944921533f
BernaIkiz54 <brn.ikiz.35@gmail.com>
2019-05-11 16:35:49 +0300

    Database bağlantısı icin ayar.php ve Robots,googlebotların index sayfasına eklenmesi

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="revisit-after" content="7 Days">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $SiteTitle; ?></title>
+    <meta name="description" content="<?php echo $SiteDescription ?>">
+    <meta name="keywords" content="<?php echo $SiteKeywords ?>">
+    <script type="text/javascript" src="Frameworks/JQuery/jquery-3.3.1.min.js" language="javascript"></script>
+    <link type="text/css" rel="stylesheet" href="Ayarlar/stil.css">
+    <script type="text/javascript" src="Ayarlar/fonksiyonlar.js" language="javascript"></script>
+</head>
+<body>
+<table width="1065" height="100%" align="center" border="0" cellpadding="0" cellspacing="0">
+    <tr height="40px" bgcolor="353745">
+        <td><img src="Resimler/HeaderMesajResmi.png" border="0"></td>
+    </tr>
+    <tr height="110">
+        <td>
+            <table width="1065" height="30" align="center" border="0" cellpadding="0" cellspacing="0">
+                <tr bgcolor="#0088CC">
+                    <td>&nbsp;</td>
+                    <td width="20px"><img src="Resimler/KullaniciBeyaz16x16.png" style="margin-top: 5px;" border="0">
+                    </td>
+                    <td width="70" class="MaviAlanMenusu"><a href="xxxxxx">Giriş Yap</a></td>
+                    <td width="20px"><img src="Resimler/KullaniciEkleBeyaz16x16.png" style="margin-top: 5px;"
+                                          border="0"></td>
+                    <td width="85" class="MaviAlanMenusu"><a href="xxxxxx">Yeni Üye Ol</a></td>
+                    <td width="20px"><img src="Resimler/SepetBeyaz16x16.png" style="margin-top: 5px;" border="0"></td>
+                    <td width="103" class="MaviAlanMenusu"><a href="xxxxxx">Alışveriş Sepeti</a></td>
+                </tr>
+            </table>
+            <table width="1065" height="80" align="center" border="0" cellpadding="0" cellspacing="0">
+                <tr bgcolor="#353745">
+                    <td width="208" style="padding-left: 120px;">Anasayfa</td>
+                    <td width="207">Mobilya</td>
+                    <td width="200">Mutfak</td>
+                    <td width="203">Ev Dekorasyon</td>
+                    <td width="213">Aydınlatma</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td>BANNER</td>
+    </tr>
+    <tr>
+        <td>İÇERİK</td>
+    </tr>
+    <tr>
+        <td>FOOTER</td>
+    </tr>
+</table>
+</body>
+</html>
+<?php
+$VeritabaniBaglantisi = null;
+?>
