c220faf797af8d0f1cea9eea9c4f5df475cf8e0f
BernaIkiz54 <brn.ikiz.35@gmail.com>
2019-05-13 23:26:31 +0300

    üyegiris islemi saglandi

--- sifremiunuttumsonuc.php
+++ sifremiunuttumsonuc.php
@@ -1,7 +1,70 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Casper
- * Date: 13.5.2019
- * Time: 20:15
- */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'Frameworks/PHPMailer/src/Exception.php';
+require 'Frameworks/PHPMailer/src/PHPMailer.php';
+require 'Frameworks/PHPMailer/src/SMTP.php';
+
+if(isset($_POST["EmailAdresi"])){
+    $GelenEmailAdresi		=	Guvenlik($_POST["EmailAdresi"]);
+}else{
+    $GelenEmailAdresi		=	"";
+}
+if(isset($_POST["TelefonNumarasi"])){
+    $GelenTelefonNumarasi	=	Guvenlik($_POST["TelefonNumarasi"]);
+}else{
+    $GelenTelefonNumarasi	=	"";
+}
+
+if(($GelenEmailAdresi!="") or ($GelenTelefonNumarasi!="")){
+    $KontrolSorgusu		=	$VeritabaniBaglantisi->prepare("SELECT * FROM uyeler WHERE EmailAdresi = ? OR TelNo = ? AND 	aktif_mi = ?");
+    $KontrolSorgusu->execute([$GelenEmailAdresi, $GelenTelefonNumarasi, 0]);
+    $KullaniciSayisi	=	$KontrolSorgusu->rowCount();
+    $KullaniciKaydi		=	$KontrolSorgusu->fetch(PDO::FETCH_ASSOC);
+
+    if($KullaniciSayisi>0){
+        $MailIcerigiHazirla		=	"Merhaba Sayın " . $KullaniciKaydi["ad_soyad"] . "<br /><br />Sitemiz üzerinde bulunan hesabınızın şifresini sıfırlamak için lütfen <a href='" . $SiteLinki . "/index.php?SK=43&AktivasyonKodu=" . $KullaniciKaydi["AktivasyonDurum"] . "&Email=" . $KullaniciKaydi["email"] . "'>BURAYA TIKLAYINIZ</a>.<br /><br />Saygılarımızla, iyi çalışmalar...<br />" . $SiteAdi;
+
+        $MailGonder		=	new PHPMailer(true);
+
+        try{
+            $MailGonder->SMTPDebug			=	0;
+            $MailGonder->isSMTP();
+            $MailGonder->Host				=	DonusumleriGeriDondur($SiteEmailHostAdresi);
+            $MailGonder->SMTPAuth			=	true;
+            $MailGonder->CharSet			=	"UTF-8";
+            $MailGonder->Username			=	DonusumleriGeriDondur($SiteEmailAdresi);
+            $MailGonder->Password			=	DonusumleriGeriDondur($SiteEmailSifresi);
+            $MailGonder->SMTPSecure			=	'tls';
+            $MailGonder->Port				=	587;
+            $MailGonder->SMTPOptions		=	array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+            $MailGonder->setFrom(DonusumleriGeriDondur($SiteEmailAdresi), DonusumleriGeriDondur($SiteAdi));
+            $MailGonder->addAddress(DonusumleriGeriDondur($KullaniciKaydi["email"]), DonusumleriGeriDondur($KullaniciKaydi["ad_soyad"]));
+            $MailGonder->addReplyTo(DonusumleriGeriDondur($SiteEmailAdresi), DonusumleriGeriDondur($SiteAdi));
+            $MailGonder->isHTML(true);
+            $MailGonder->Subject			=	$SiteAdi . ' Şifre Sıfırlama';
+            $MailGonder->MsgHTML($MailIcerigiHazirla);
+            $MailGonder->send();
+
+            header("Location:index.php?SK=39");
+            exit();
+        }catch(Exception $e){
+            header("Location:index.php?SK=40");
+            exit();
+        }
+    }else{
+        header("Location:index.php?SK=41");
+        exit();
+    }
+}else{
+    header("Location:index.php?SK=42");
+    exit();
+}
+?>
--- yenisifreolustursonuctamam.php
+++ yenisifreolustursonuctamam.php
@@ -3,13 +3,13 @@
 		<td>&nbsp;</td>
 	</tr>
 	<tr height="100">
-		<td align="center"><img src="Resimler/Dikkat.png" border="0"></td>
+		<td align="center"><img src="Resimler/Tamam.png" border="0"></td>
 	</tr>
 	<tr height="50">
-		<td align="center"><b>DİKKAT. Şifre Sıfırlama Formunda Eşleşmeyen Şifreler.</b></td>
+		<td align="center"><b>TEBRİKLER. Yeni Şifre Başarıyla Oluşturuldu.</b></td>
 	</tr>
 	<tr>
-		<td align="center">Şifre sıfırlama formu dahilinde yazmış olduğunuz şifreler eşleşmemektedir.</td>
+		<td align="center">Üyeliğinize ait hesap şifreniz isteğiniz doğrultusunda güncellenmiştir.</td>
 	</tr>
 	<tr>
 		<td class="SonucSayfalari" align="center">Ana sayfaya dönmek için lütfen buraya <a href="index.php"><b>tıklayınız.</b></a></td>
